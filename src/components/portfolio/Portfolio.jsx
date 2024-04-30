21602a0c2c1e13bd281f8e414f60f9e2ae0e86c5
Raksha Padiyar <rakshapadiyar100@gmail.com>
2024-05-01 00:51:59 +0530

    projet added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./portfolio.css";
 import qr from "../../assets/qr.png";
 import gcp from "../../assets/gcloud.png";
-import factbase from "../../assets/factbase.png";
-import chitchat from "../../assets/chitchat.png";
+import staticServe from "../../assets/static-serve.png";
+import portfolio from "../../assets/portfolio.png";
+import slack from "../../assets/slack.png";
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -13,49 +14,73 @@ const Portfolio = () => {
       <div className="container project__container">
         <article className="project">
           <div className="project__image">
-            <img src={factbase} alt="gcp" />
+            <img src={staticServe} alt="gcp" height="100%" />
           </div>
-          <h5 className="organisation">FactBase </h5>
-          <small className="project__detail">Frontend Project</small>
-          <a
-            href="https://github.com/rakshapadiyar/Factbase"
-            target="_blank"
-            className="project-link"
-            rel="noreferrer"
-          >
-            Repo
-          </a>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={chitchat} alt="gcp" />
-          </div>
-          <h5 className="organisation">ChitChat</h5>
-          <small className="project__detail">Backend Project</small>
-          <a
-            href="https://github.com/rakshapadiyar/ChitChat"
-            target="_blank"
-            className="project-link"
-            rel="noreferrer"
-          >
-            Repo
-          </a>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={gcp} alt="aws" />
-          </div>
-          <h5 className="organisation"> IaaS Project </h5>
+          <h5 className="organisation">Static Serve </h5>
           <small className="project__detail">Cloud Project</small>
           <a
-            href="https://github.com/rakshapadiyar/GCP-Cloud-Project"
+            href="https://github.com/rakshapadiyar/Static-Serve"
             target="_blank"
             className="project-link"
             rel="noreferrer"
           >
             Repo
+          </a>
+          <a
+            href="https://www.youtube.com/shorts/uY2q-SvgM8E?feature=share"
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Demo
+          </a>
+        </article>
+
+        <article className="project">
+          <div className="project__image">
+            <img src={portfolio} alt="react" height="100%" />
+          </div>
+          <h5 className="organisation">Portfolio</h5>
+          <small className="project__detail">FrontEnd Project</small>
+          <a
+            href="https://github.com/rakshapadiyar/Portfolio"
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Repo
+          </a>
+          <a
+            href="https://rakshapadiyar.github.io/Portfolio/"
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Demo
+          </a>
+        </article>
+
+        <article className="project">
+          <div className="project__image">
+            <img src={slack} alt="gcp" />
+          </div>
+          <h5 className="organisation"> GitHub-Slack Interation </h5>
+          <small className="project__detail">Cloud Project</small>
+          <a
+            href="https://github.com/rakshapadiyar/GitHub-Slack-Integration"
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Repo
+          </a>
+          <a
+            href="https://www.youtube.com/watch?v=8BDUfGBvma8"
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Demo
           </a>
         </article>
         <article className="project">
