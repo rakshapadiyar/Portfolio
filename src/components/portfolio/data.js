88ab886aa00b93299a8e0c1f6fd19d039d6f38cb
Raksha Padiyar <rakshapadiyar100@gmail.com>
2024-05-01 15:04:35 +0530

    portfolio pic change

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -1,17 +1,18 @@
 import stServe from "../../assets/static-serve.png";
 import slack from "../../assets/slack.png";
+import portfolio from "../../assets/portfolio.png"
 
 const projects = [
   {
     name: "Static Serve",
-    area: "Google Cloud",
+    area: "Google Cloud - Cloud Storage",
     repo: "https://github.com/rakshapadiyar/Static-Serve",
     demo: "https://www.youtube.com/shorts/uY2q-SvgM8E",
     img: stServe,
   },
   {
     name: "GitHub-Slack Integration",
-    area: "Google Cloud",
+    area: "Google Cloud - Cloud Functions",
     repo: "https://github.com/rakshapadiyar/GitHub-Slack-Integration",
     demo: "https://www.youtube.com/watch?v=8BDUfGBvma8",
     img: slack,
@@ -21,7 +22,7 @@ const projects = [
     name: "Portfolio Project",
     repo: "https://github.com/rakshapadiyar/Portfolio",
     demo: "rakshapadiyar.github.io/Portfolio/",
-    img: slack,
+    img: portfolio,
   },
 ];
 
