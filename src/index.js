270dd3518fce6920cc46d723918d81a31ea6bc7a
Raksha Padiyar <rakshapadiyar100@gmail.com>
2024-05-01 14:43:22 +0530

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import "react-tooltip/dist/react-tooltip.css";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
